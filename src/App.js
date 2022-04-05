a1c3401e34f8fa71185000eaea86aaa6fbdd7b4c
giorgi gujejiani <gujejiani.websmart@gmail.com>
2022-04-05 20:00:44 +0400

    finished

--- src/App.js
+++ src/App.js
@@ -42,7 +42,6 @@ const App = () => {
           todos={todos}
           todosPerPage={todosPerPage}
         />
-        gamarhoba
       </div>
     </>
   );
--- src/components/singleTodo/Todo.js
+++ src/components/singleTodo/Todo.js
@@ -1,51 +1,45 @@
+import React, {useRef} from "react";
 import {Button} from "react-bootstrap";
-import React from "react";
 
-
-const Todo = ({todos, start, end, setEditInputValue, checkboxHandler, removeHandler, editHandler}) => {
+const Todo = ({item, checkboxHandler, removeHandler, editHandler}) => {
+	const inputRef = useRef('')
 	return (
-		<>
-			{todos.slice(start, end)?.map((item) => {
-				return (
-					<div
-						key={item.id}
-						className="bg-success bg-opacity-10 rounded-3 p-lg-2 d-flex mt-3 align-items-center"
-					>
-						{item.editMode ? (
-							<input
-								defaultValue={item.task}
-								onChange={(e) => setEditInputValue(e.target.value)}
-							/>
-						) : (
-							<span
-								className={`todoTag ${item.done ? 'lineThrougher' : ''}`}
-
-							>
+		<div
+			key={item.id}
+			className="bg-success bg-opacity-10 rounded-3 p-lg-2 d-flex mt-3 align-items-center"
+		>
+			{item.editMode ? (<input
+				ref={inputRef}
+				defaultValue={item.task}
+				// onChange={setEditInputValue(inputRef.current?.value)}
+			/>) : (<span
+				style={{
+					fontSize: "24px", minWidth: "100px", textDecoration: item.done ? "line-through" : "",
+				}}
+			>
                 {item.task}
-              </span>
-						)}
-						<input onChange={() => checkboxHandler(item.id)} type="checkbox" />
-						<Button
-							onClick={() => removeHandler(item.id)}
-							className="m-lg-2"
-							size="sm"
-							variant="danger"
-						>
-							{item.editMode ? "cancel" : "remove"}
-						</Button>
-						<Button
-							onClick={() => editHandler(item.id)}
-							className=""
-							size="sm"
-							variant="info"
-						>
-							{item.editMode ? "save" : "edit"}
-						</Button>
-					</div>
-				);
-			})}
-		</>
-	)
+              </span>)}
+			<input onChange={() => checkboxHandler(item.id)} type="checkbox"/>
+			<Button
+				onClick={() =>  removeHandler(item.id) }
+				className="m-lg-2"
+				size="sm"
+				variant="danger"
+			>
+				{item.editMode ? "cancel" : "remove"}
+			</Button>
+			<Button
+				onClick={() =>  editHandler(item.id, inputRef.current?.value)  }
+				className=""
+				size="sm"
+				variant="info"
+			>
+				{item.editMode ? "save" : "edit"}
+			</Button>
+		</div>)
+
+
+
 }
 
 export default Todo
