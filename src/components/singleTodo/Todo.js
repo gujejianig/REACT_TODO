e411ba320a26b9114620783055f3d15d0ec1dbf2
giorgi gujejiani <gujejiani.websmart@gmail.com>
2022-04-05 20:22:23 +0400

    added key

--- src/components/singleTodo/Todo.js
+++ src/components/singleTodo/Todo.js
@@ -1,45 +1,46 @@
-import React, {useRef} from "react";
-import {Button} from "react-bootstrap";
+import React, { useRef } from "react";
+import { Button } from "react-bootstrap";
 
-const Todo = ({item, checkboxHandler, removeHandler, editHandler}) => {
-	const inputRef = useRef('')
-	return (
-		<div
-			key={item.id}
-			className="bg-success bg-opacity-10 rounded-3 p-lg-2 d-flex mt-3 align-items-center"
-		>
-			{item.editMode ? (<input
-				ref={inputRef}
-				defaultValue={item.task}
-				// onChange={setEditInputValue(inputRef.current?.value)}
-			/>) : (<span
-				style={{
-					fontSize: "24px", minWidth: "100px", textDecoration: item.done ? "line-through" : "",
-				}}
-			>
-                {item.task}
-              </span>)}
-			<input onChange={() => checkboxHandler(item.id)} type="checkbox"/>
-			<Button
-				onClick={() =>  removeHandler(item.id) }
-				className="m-lg-2"
-				size="sm"
-				variant="danger"
-			>
-				{item.editMode ? "cancel" : "remove"}
-			</Button>
-			<Button
-				onClick={() =>  editHandler(item.id, inputRef.current?.value)  }
-				className=""
-				size="sm"
-				variant="info"
-			>
-				{item.editMode ? "save" : "edit"}
-			</Button>
-		</div>)
+const Todo = ({ item, checkboxHandler, removeHandler, editHandler }) => {
+  const inputRef = useRef("");
+  return (
+    <div className="bg-success bg-opacity-10 rounded-3 p-lg-2 d-flex mt-3 align-items-center">
+      {item.editMode ? (
+        <input
+          ref={inputRef}
+          defaultValue={item.task}
+          // onChange={setEditInputValue(inputRef.current?.value)}
+        />
+      ) : (
+        <span
+          style={{
+            fontSize: "24px",
+            minWidth: "100px",
+            textDecoration: item.done ? "line-through" : "",
+          }}
+        >
+          {item.task}
+        </span>
+      )}
+      <input onChange={() => checkboxHandler(item.id)} type="checkbox" />
+      <Button
+        onClick={() => removeHandler(item.id)}
+        className="m-lg-2"
+        size="sm"
+        variant="danger"
+      >
+        {item.editMode ? "cancel" : "remove"}
+      </Button>
+      <Button
+        onClick={() => editHandler(item.id, inputRef.current?.value)}
+        className=""
+        size="sm"
+        variant="info"
+      >
+        {item.editMode ? "save" : "edit"}
+      </Button>
+    </div>
+  );
+};
 
-
-
-}
-
-export default Todo
+export default Todo;
--- src/components/todoList/TodosList.js
+++ src/components/todoList/TodosList.js
@@ -1,79 +1,91 @@
-import {Button} from "react-bootstrap";
-import React, {useEffect, useState} from "react";
+import { Button } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
 import Todo from "../singleTodo/Todo";
 
 const TodosList = ({
-	                   todos, setTodos, todosPerPage, activePage, setActivePage, lastBtnPagination,
-                   }) => {
-	const [editing, setEditing] = useState(false); // controls if edit mode is on/off
-	const [editInputValue, setEditInputValue] = useState(""); // for editing input Value
+  todos,
+  setTodos,
+  todosPerPage,
+  activePage,
+  setActivePage,
+  lastBtnPagination,
+}) => {
+  const [editing, setEditing] = useState(false); // controls if edit mode is on/off
+  const [editInputValue, setEditInputValue] = useState(""); // for editing input Value
 
-	// remove Task from the list
-	const removeHandler = (id) => {
-		if (!editing) {
-			setTodos(todos.filter((todo) => {
-				return todo.id !== id;
-			}));
-		} else {
-			const arr = todos.map((todo) => {
-				if (todo.id === id) {
-					todo.editMode = false;
-					setEditing(false);
-				}
-				return todo;
-			});
-			setTodos(arr);
-		}
+  // remove Task from the list
+  const removeHandler = (id) => {
+    if (!editing) {
+      setTodos(
+        todos.filter((todo) => {
+          return todo.id !== id;
+        })
+      );
+    } else {
+      const arr = todos.map((todo) => {
+        if (todo.id === id) {
+          todo.editMode = false;
+          setEditing(false);
+        }
+        return todo;
+      });
+      setTodos(arr);
+    }
 
-		// // Reduce pagination by one when the last item is deleted from the last page
-		if ((todos.length - 1) % 5 === 0 && activePage === lastBtnPagination) {
-			setActivePage(activePage - 1);
-		}
-	};
+    // // Reduce pagination by one when the last item is deleted from the last page
+    if ((todos.length - 1) % 5 === 0 && activePage === lastBtnPagination) {
+      setActivePage(activePage - 1);
+    }
+  };
 
-	const editHandler = (id, inputValue) => {
-		const selected = todos.map((todo) => {
-			if (todo.id === id) {
-				!todo.editMode ? (todo.editMode = true) : (todo.editMode = false);
-				if (todo.editMode) {
-					setEditInputValue(todo.task);
-					setEditing(true);
-				}
-				else
-				{
-					console.log('save==>', inputValue)
-					todo.task = inputValue;
-						setEditing(false);
-				}
-			}
+  const editHandler = (id, inputValue) => {
+    const selected = todos.map((todo) => {
+      if (todo.id === id) {
+        !todo.editMode ? (todo.editMode = true) : (todo.editMode = false);
+        if (todo.editMode) {
+          setEditInputValue(todo.task);
+          setEditing(true);
+        } else {
+          console.log("save==>", inputValue);
+          todo.task = inputValue;
+          setEditing(false);
+        }
+      }
 
-			return todo;
-		});
-		setTodos(selected);
-	};
-	// console.log(editInputValue);
-	const checkboxHandler = (id) => {
-		const selectedTodo = todos.map((todo) => {
-			if (todo.id === id) {
-				!todo.done ? (todo.done = true) : (todo.done = false);
-			}
-			return todo;
-		});
-		setTodos(selectedTodo);
-	};
+      return todo;
+    });
+    setTodos(selected);
+  };
+  // console.log(editInputValue);
+  const checkboxHandler = (id) => {
+    const selectedTodo = todos.map((todo) => {
+      if (todo.id === id) {
+        !todo.done ? (todo.done = true) : (todo.done = false);
+      }
+      return todo;
+    });
+    setTodos(selectedTodo);
+  };
 
-	//activePage is responsible for rendering items correctly
-	let end = activePage * todosPerPage;
-	let start = end - todosPerPage;
-	return (<>
-			{todos.slice(start, end)?.map((item) => {
-				return <Todo item={item}
-				             setEditInputValue={setEditInputValue}
-				             checkboxHandler={checkboxHandler}
-				             editHandler={editHandler}
-				             removeHandler={removeHandler}/>
-			})}
-		</>);
+  //activePage is responsible for rendering items correctly
+  let end = activePage * todosPerPage;
+  let start = end - todosPerPage;
+  return (
+    <>
+      {todos.slice(start, end)?.map((item) => {
+        return (
+          <Todo
+            key={item.id}
+            item={item}
+            setEditInputValue={setEditInputValue}
+            checkboxHandler={checkboxHandler}
+            editHandler={editHandler}
+            removeHandler={removeHandler}
+          />
+        );
+      })}
+    </>
+  );
 };
 
 export default TodosList;
